5fb87618f462c14187e0ab3a5dfddf7fa2d01596
jerenteria <renteria.juan96@gmail.com>
2024-02-10 13:04:43 -0800

    added useRef to get user input data in order to display player name

--- section8_app/src/components/Player.jsx
+++ section8_app/src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState, useRef } from 'react';
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+
+  // playerName stores entered value during input in ref allowing you to render it later making it easier than useState
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
+  };
+
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {/* if enteredPlayerName is valid(not an empty string) then render it if not then render 'unkown entity' */}
+      <h2>Welcome {enteredPlayerName ? enteredPlayerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text"/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
